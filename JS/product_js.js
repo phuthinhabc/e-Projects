05c222026f20ca00bf2b7e650ed9cc72c2be28d5
Unknown <phuthinhabc@gmail.com>
2018-08-13 21:24:00 +0700

    Add new folder and image
    Add new image folder, modify product page

--- JS/product_js.js
+++ JS/product_js.js
@@ -1,70 +1,17 @@
 let myApp = angular.module('myApp',[]);
-myApp.controller('myAppController',
-function($scope)
-{
-
-    $scope.products=[
+myApp.controller('myAppController',function($scope){
+    $scope.data=[
         {
-            'id': 1,
-            'name': 'BMW - 320i',
-            'year': 2010,
-            'company': 'BMW',
-            'info': 'lorem ipsum',
-            'price': 250000
+            name: 'Dining table',
+            imageUrl: '/images/products/dining.jpg',
+            model: '2017',
+            manufacture: 'DURIAN'
         },
         {
-            'id': 2,
-            'name': 'BMW - 520i',
-            'year': 2010,
-            'company': 'BMW',
-            'info': 'lorem ipsum dolor sit amet',
-            'price': 250000
+            name: 'Desk',
+            imageUrl: '/images/products/dining.jpg',
+            model: '2018',
+            manufacture: 'DURIAN'
         },
-        {
-            'id': 3,
-            'name': 'BMW - 520i',
-            'year': 2010,
-            'company': 'BMW',
-            'info': 'lorem ipsum dolor sit amet',
-            'price': 250000
-        },
-        {
-            'id': 4,
-            'name': 'BMW - 520i',
-            'year': 2010,
-            'company': 'BMW',
-            'info': 'lorem ipsum dolor sit amet',
-            'price': 250000
-        },
-        {
-            'id': 5,
-            'name': 'BMW - 520i',
-            'year': 2010,
-            'company': 'BMW',
-            'info': 'lorem ipsum dolor sit amet',
-            'price': 250000
-        },
-
     ];
-
-    $scope.product_element = [];
-    for (let i of $scope.products)
-    {
-        for(let n in i)
-        {
-            $scope.product_element.push(n);
-        }
-    }
-    $scope.unique_product_element = [];
-    $.each($scope.product_element,function(i, el)
-        {
-            if($.inArray(el, $scope.unique_product_element) === -1) 
-            {
-                $scope.unique_product_element.push(el);
-            }   
-        }
-    );
-    $scope.unique_product_element = $scope.unique_product_element.splice(1,$scope.unique_product_element.length);
-
-}
-);
+});
