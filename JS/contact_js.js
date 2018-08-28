17cd84236ab39082bfe2336e554d851308e2f0bf
Unknown <phuthinhabc@gmail.com>
2018-08-28 09:53:36 +0700

    Update contact page

--- JS/contact_js.js
+++ JS/contact_js.js
@@ -1,3 +1,29 @@
 (function blink() { 
   $('.blink_me').fadeOut(500).fadeIn(500, blink); 
 })();
+
+
+
+var captcha;
+ 
+function generateCaptcha() {
+    var a = Math.floor((Math.random() * 10));
+    var b = Math.floor((Math.random() * 10));
+    var c = Math.floor((Math.random() * 10));
+    var d = Math.floor((Math.random() * 10));
+ 
+    captcha=a.toString()+b.toString()+c.toString()+d.toString();
+    
+    document.getElementById("captcha").value = captcha;
+}
+ 
+function check(){
+    var input=document.getElementById("inputText").value;
+ 
+    if(input==captcha){
+        alert("Your feedback has been send to us!");
+    }
+    else{
+        alert("Wrong captcha");
+    }
+}	
